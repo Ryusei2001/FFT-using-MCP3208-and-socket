7169cca90ea29acb21e127e8cbb0e59d1ed0ca85
Ryusei2001 <dragonstar0115@gmail.com>
2022-09-15 15:01:54 +0900

    update

---
+++ signalTest.py
@@ -0,0 +1,9 @@
+import signal
+import time
+
+def scheduler(arg1, args2):
+    print(time.time())
+
+signal.signal(signal.SIGALRM, scheduler)
+signal.setitimer(signal.ITIMER_REAL, 0.001, 0.01)
+time.sleep(1000)
