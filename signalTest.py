33b9496ddae34385b8e7504c7d2be77c4a65385a
Ryusei2001 <dragonstar0115@gmail.com>
2022-09-16 22:04:52 +0900

    update signal function

--- signalTest.py
+++ signalTest.py
@@ -1,9 +1,33 @@
 import signal
+import threading
 import time
+from time import sleep
 
-def scheduler(arg1, args2):
-    print(time.time())
+def callback(arg1, args2):
+	global i
+	#print(time.time())
+	print(i)
+	if i >= 128:
+		signal.alarm(0)
+	i = i + 1
 
-signal.signal(signal.SIGALRM, scheduler)
-signal.setitimer(signal.ITIMER_REAL, 0.001, 0.01)
-time.sleep(1000)
+
+def timer():
+	while True:
+		global i
+		i = 0
+		signal.signal(signal.SIGALRM, callback)
+		signal.setitimer(signal.ITIMER_REAL, 0.1, 0.007)
+		time.sleep(1)
+
+		#timer2()
+		#timer3()
+		#time.sleep(5)
+
+def timer2():
+	print("hello")
+
+def timer3():
+	print("signal")
+
+timer()
