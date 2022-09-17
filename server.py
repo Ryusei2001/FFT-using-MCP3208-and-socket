bb54c3508c0dc8737253d748402e6ad955af93a3
Ryusei2001 <dragonstar0115@gmail.com>
2022-09-17 14:25:32 +0900

    use signal for ADC

--- server.py
+++ server.py
@@ -5,24 +5,23 @@ import struct
 import math
 import numpy as np
 import time
+from time import sleep
 import signal
 
 class BlockingServerBase:
 	def __init__(self, timeout=60, buffer=1024):
 		global adc
-		global CurrentBuffer
 		global nSample
-		global A, B
+		global BufferA
 		adc = MCP3208(channel=0, differential=False)
-		CurrentBuffer = -1
 		nSample = 128
+		self.close()
+		Vref = 3.3
+		BufferA = np.array([])
 		self.__socket = None
 		self.__timeout = timeout
 		self.__buffer = buffer
 		self.close()
-		Vref = 3.3
-		A = np.array([])
-		B = np.array([])
 
 	def __del__(self):
 		self.close()
@@ -48,12 +47,9 @@ class BlockingServerBase:
 				message_recv = conn.recv(self.__buffer).decode('utf-8')
 				message_resp = self.respond(message_recv)
 				#conn.send(message_resp.encode('utf-8'))
+				self.measurement()
 				for i in range(nSample):
-					if CurrentBuffer == 0:
-						str = str + ('{:1.5f}'.format(A[i]) + ",")
-					else:
-						str = str + ('{:1.5f}'.format(B[i]) + ",")
-
+						str = str + ('{:1.5f}'.format(BufferA[i]) + ",")
 				conn.send(str.encode('utf-8'))
 			except ConnectionResetError:
 				break
@@ -61,26 +57,22 @@ class BlockingServerBase:
 				break
 		self.close()
 
-	def measurement_callback(self):
-		global CurrentBuffer
-		global A
-		global B
-		if CurrentBuffer != 0:
-			for i in range(nSample):
-				#A[i] = adc.value
-				A = np.append(A, adc.value)
-			CurrentBuffer = 0
-			time.sleep(1)
+	def measurement_callback(self, args1, args2):
+		global CallbackCount
+		global BufferA
+		global adc
+		BufferA = np.append(BufferA, adc.value)
+		if CallbackCount >= 128:
+			signal.alarm(0)
 		else:
-			for i in range(nSample):
-				B = np.append(B, adc.value)
-			CurrentBuffer = 1
-			time.sleep(1)
+			CallbackCount = CallbackCount + 1
 
 	def measurement(self):
-		signal.signal(signal.SIGALRM, measurement_callback)
-		signal.setitimer(signal.ITIMER_REAL, 0.001, 0.01)
-		time.sleep(1000)
+		global CallbackCount
+		CallbackCount = 0
+		signal.signal(signal.SIGALRM, self.measurement_callback)
+		signal.setitimer(signal.ITIMER_REAL, 0.1, 0.007)
+		time.sleep(1)
 
 	def respond(self, message:str) -> str:
 		return ""
@@ -89,7 +81,6 @@ class InetServer(BlockingServerBase):
 	def __init__(self, host:str="0.0.0.0", port:int=8080) -> None:
 		self.server=(host,port)
 		super().__init__(timeout=60, buffer=1024)
-		self.measurement()
 		self.accept(self.server, socket.AF_INET, socket.SOCK_STREAM, 0)
 
 	def respond(self, message:str) -> str:
