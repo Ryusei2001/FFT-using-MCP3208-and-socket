2b5156feb68b55b4f4c0f2d457feec914ee5a230
Ryusei2001 <93074989+Ryusei2001@users.noreply.github.com>
2022-10-08 20:30:03 +0900

    FFTデータ個数不足時の動作を追加

--- readAnalog.py
+++ readAnalog.py
@@ -13,8 +13,8 @@ class readAnalog:
 		global SignalFrequency
 		global BufferA
 		#adc = MCP3208(channel=0, differential=False)
-		nSample = 128
-		SignalFrequency = 50
+		nSample = 4096	
+		SignalFrequency = 800
 		BufferA = np.array([])
 
 	def main(self):
@@ -22,7 +22,8 @@ class readAnalog:
 		global BufferA
 		while True:
 			self.measurement()
-			self.FFT(BufferA, nSample, SignalFrequency)
+			FFTrow = len(BufferA)
+			self.FFT(BufferA, FFTrow, SignalFrequency)
 
 	def measurement_callback(self, args1, args2):
 		global CallbackCount
@@ -32,7 +33,7 @@ class readAnalog:
 		#volt = np.round(adc.value * Vref, 5)
 		volt = random.random()
 		BufferA = np.append(BufferA, volt)
-		if CallbackCount >= 127:
+		if CallbackCount >= nSample - 1:
 			print("Finished measurement with {} rows!".format(len(BufferA)))
 			signal.alarm(0)
 		else:
@@ -44,22 +45,23 @@ class readAnalog:
 		CallbackCount = 0
 		BufferA = np.zeros(0)
 		signal.signal(signal.SIGALRM, self.measurement_callback)
-		signal.setitimer(signal.ITIMER_REAL, 0.1, 0.02)
+		signal.setitimer(signal.ITIMER_REAL, 0.1, 1 / SignalFrequency)
 
-		while CallbackCount < 127:
+		while CallbackCount < nSample - 1:
 			time.sleep(0.5)
 
 	def FFT(self, value: list, FFTrow: int, freq: int):
 		if list(bin(FFTrow)).count('1') != 1:
 			print ("The number must be a power of 2. This is {}".format(FFTrow))
-			return
-		SpectrumAmplitude = [0.0] * FFTrow
-		Freqency = [0.0] * FFTrow
-		FFT = np.fft.fft(value[0:0+FFTrow])
-		for i in range(FFTrow):
-			SpectrumAmplitude[i] = np.sqrt(
-				FFT[i].real * FFT[i].real + FFT[i].imag * FFT[i].imag)
-			Freqency[i] = (i * freq) / FFTrow
+			return False
+		else:
+			SpectrumAmplitude = [0.0] * FFTrow
+			Freqency = [0.0] * FFTrow
+			FFT = np.fft.fft(value[0:FFTrow])
+			for i in range(FFTrow):
+				SpectrumAmplitude[i] = np.sqrt(
+					FFT[i].real * FFT[i].real + FFT[i].imag * FFT[i].imag)
+				Freqency[i] = (i * freq) / FFTrow
 
 		plt.subplot(2,1,1)
 		plt.plot(BufferA, color="b", linewidth=1.0, linestyle="-") 
@@ -69,7 +71,7 @@ class readAnalog:
 
 		plt.subplot(2,1,2)
 		plt.plot(Freqency, SpectrumAmplitude, color="b", linewidth=1.0, linestyle="-") 
-		plt.xlim(0, SignalFrequency / 2)
+		plt.xlim(0, freq / 2)
 		plt.ylim(0, 30)
 		plt.title("Freqency spectrum", fontsize=14, fontname='serif') 
 		plt.xlabel("Freqency [Hz]", fontsize=14, fontname='serif') 
